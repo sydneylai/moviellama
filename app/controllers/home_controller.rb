6c45bca02ba65c1c141306e66ba1d3be5bfe1492
Lynn Wang <lynncwang@gmail.com>
2014-11-05 01:36:29 -0800

    modified homecontroller for search query updates

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,12 @@
 class HomeController < ApplicationController
   def index
   	@q = params[:q]
+  	if(@q == "" || @q == nil)
+  		@movies = Movie.all
+  	else
+  		@movies = Movie.where("title like ? ", "%#{@q}%")
+  	end
+  	render json: @movies
   end
+
 end
