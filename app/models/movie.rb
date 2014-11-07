92f87eda4beac42af341a0645deee4c7ec1fd151
Sasha <sashahouse111@gmail.com>
2014-11-06 21:15:09 -0800

    changed tagline and llama rating content

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -41,9 +41,9 @@ class Movie < ActiveRecord::Base
 		if imdbscore == nil or rtscore == nil
 			return "Llama doesn't know" 
 		elsif imdbscore.rating > 7.0 and rtscore.rating > 80
-			return "Llama pass!"
+			return "Llama Says: PASS!"
 		else
-			return "Llama fail!"
+			return "Llama Says: FAIL!"
 		end
 	end
 
