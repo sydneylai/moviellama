b888fc887482d8755f577e3016ac0571f32cba49
Lynn Wang <lynncwang@gmail.com>
2014-11-08 18:33:26 -0800

    changed to string

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141108215818) do
+ActiveRecord::Schema.define(version: 20141109023053) do
 
   create_table "movies", force: true do |t|
     t.string   "title"
@@ -38,7 +38,7 @@ ActiveRecord::Schema.define(version: 20141108215818) do
   create_table "sources", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "url"
+    t.text     "url",        limit: 255
     t.string   "name"
     t.integer  "movie_id"
     t.float    "price"
